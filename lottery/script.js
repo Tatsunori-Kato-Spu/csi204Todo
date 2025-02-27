b42f2d908b1657627f3f55a7555e90d53a95e7c1
TatsunoriSPU <tatsunori.kat@spumail.net>
2025-02-27 10:20:41 +0700

    fix

--- lottery/script.js
+++ lottery/script.js
@@ -2,7 +2,6 @@ function generateLottery() {
     const numCount = document.getElementById('numCount').value;
     const lotteryNumbers = [];
     
-    // Generate unique random numbers between 1 and 49
     while (lotteryNumbers.length < numCount) {
         let randomNum = Math.floor(Math.random() * 49) + 1;
         if (!lotteryNumbers.includes(randomNum)) {
@@ -10,6 +9,5 @@ function generateLottery() {
         }
     }
 
-    // Display the generated numbers
     document.getElementById('lotteryNumbers').textContent = lotteryNumbers.join(', ');
 }
